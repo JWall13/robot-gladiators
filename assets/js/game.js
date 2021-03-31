ed30aa276f1a873b2677c1683567921f916d0bde
Jeremiah Wallace <jeremiah@jeremiahwallace.com>
2021-03-31 09:05:56 -0500

    trying to get help

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,15 +10,15 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-
-
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-  ...
-}
+  /* repeat and execute as long as the enemy-robot is alive */
+  while(enemyHealth > 0) {
+    /* place fight function code block here . . . */
+  }
 
 for(var i = 0; i < enemyNames.length; i++) {
   console.log(enemyNames[i]);
@@ -85,6 +85,3 @@ var fight = function() {
 for(var i = 0; i < enemyNames.length; i++) {
   fight(enemyNames[i]);
 }
-
-// run fight function to start game
-// fight();
