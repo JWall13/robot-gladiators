53bdc69748f418e4313b17b3c67bcc5163c1a695
Jeremiah Wallace <jeremiah@jeremiahwallace.com>
2021-03-30 20:40:08 -0500

    failed at adding enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,30 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
 // var playerName = 'Clank McKrank';
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+
+var fight = function(enemyName) {
+  ...
+}
+
+for(var i = 0; i < enemyNames.length; i++) {
+  console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyNames[i] + " is at " + i + " index");
+}
 
 // fight function
 var fight = function() {
@@ -67,5 +82,9 @@ var fight = function() {
   }
 };
 
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
+
 // run fight function to start game
-fight();
+// fight();
